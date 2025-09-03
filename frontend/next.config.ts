6f1b2244f3a0f9157a4b0869beff4459c604ff32
rutvikraut2001 <rutvik.raut@pinnacle.in>
2025-09-03 16:54:07 +0530

    Feat : initial commit

---
+++ frontend/next.config.ts
@@ -0,0 +1,10 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  /* config options here */
+
+  // enables `next export` static export mode
+  output: 'export',
+};
+
+module.exports = nextConfig;
