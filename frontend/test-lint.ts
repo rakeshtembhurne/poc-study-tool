0ea92141f72f461db8148f4d227ce5fb6de64e1d
Nikhil Barapatre <nikhil.barapatre@pinnacle.in>
2025-09-05 12:35:52 +0530

    test: add test file for pre-commit hook validation

---
+++ frontend/test-lint.ts
@@ -0,0 +1,9 @@
+// Test file with intentional formatting issues
+const badlyFormatted = 'test';
+const unusedVariable = 'this should trigger linting';
+
+function poorlyFormatted(param1: string, param2: number) {
+  return param1 + param2;
+}
+
+export default poorlyFormatted;
